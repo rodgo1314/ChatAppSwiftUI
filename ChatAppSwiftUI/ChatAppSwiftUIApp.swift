ceb15d68c6e9f45010f1746838ae9e3a546b8216
rodgo1314 <rleyva@codingdojo.com>
2021-11-09 00:00:24 -0800

    Initial Commit

---
+++ ChatAppSwiftUI/ChatAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSwiftUIApp.swift
+//  ChatAppSwiftUI
+//
+//  Created by Rodrigo Leyva on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
